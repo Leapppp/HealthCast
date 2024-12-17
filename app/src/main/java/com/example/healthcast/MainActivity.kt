b80a9523ee45faa1667bb4b4060fb84630eecd26
Leapppp <rafaelpangestu1@gmail.com>
2024-12-17 18:00:49 +0700

    intial commit

--- app/src/main/java/com/example/healthcast/MainActivity.kt
+++ app/src/main/java/com/example/healthcast/MainActivity.kt
@@ -1,20 +1,74 @@
 package com.example.healthcast
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LocationListener {
+
+    private lateinit var buttonLocation: Button
+    private lateinit var textViewLocation: TextView
+    private lateinit var locationManager: LocationManager
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        textViewLocation = findViewById(R.id.text_location)
+        buttonLocation = findViewById(R.id.button_location)
+
+        // Runtime permissions
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 100)
+        }
+
+        buttonLocation.setOnClickListener {
+            // Create method
+            getLocation()
         }
     }
+
+    @SuppressLint("MissingPermission")
+    private fun getLocation() {
+        try {
+            locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f, this)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    override fun onLocationChanged(location: Location) {
+        Toast.makeText(this, "${location.latitude},${location.longitude}", Toast.LENGTH_SHORT).show()
+        try {
+            val geocoder = Geocoder(this, Locale.getDefault())
+            val addresses: MutableList<Address>? = geocoder.getFromLocation(location.latitude, location.longitude, 1)
+            val address = addresses?.get(0)?.getAddressLine(0)
+
+            textViewLocation.text = address
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
+
+    override fun onProviderEnabled(provider: String) {}
+
+    override fun onProviderDisabled(provider: String) {}
 }
